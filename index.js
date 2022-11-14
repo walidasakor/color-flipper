07ab7154b001f186dc9461fbba510f08c333f840
walidasakor <111765419+walidasakor@users.noreply.github.com>
2022-11-14 19:15:15 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
-const text= document.getElementById("color")
-const btn= document.getElementById ("btn")
-btn.addEventListener('click', function(){
-    document.body.style.backgroundColor=colors[getRandomColor()]
-    text.textContent=colors[getRandomColor()]
-})
+const characters = ["A","B","C","D","E","F","G","H","I",
+"J","K","L","M","N","O","P","Q","R","S","T","U","V",
+"W","X","Y","Z","a","b","c","d","e","f","g","h","i"
+ ,"j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];``
+let length=16
+password=""
+let emptyBtnOne=document.getElementById("empty-btn-1")
 
-function getRandomColor(){
-    return Math.floor(Math.random()*colors.length)
-}
+
+function generatePassword(){
+  for(i=0; i<characters.length; i++){
+    let password=Math.floor(Math.random()*characters.length) ;
+    emptyBtnOne.textContent=characters[password];
+  }
+  }
